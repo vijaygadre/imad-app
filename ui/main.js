4c7453fe5bf1803ca3c3def835d937f8b3bc8338
vijaygadre <vijayvgadre@gmail.com>
2018-02-20 16:02:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element = document.getElementById('maintext');
 element.innerHTML = 'New Value';
 
 //Move the image
-var img = document.getElementById('img');
+var img = document.getElementById('madi');
 img.onclick = function() {
     img.style.marginleft = '100px';
 };
